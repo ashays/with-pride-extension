35ce2f969bcc9b75080f4cfdfd42bd090da75b98
Ashay Sheth <ashay.sheth@gmail.com>
2020-12-15 22:57:14 -0500

    update page action icon (green, yellow, red) based on host

--- background.js
+++ background.js
@@ -1,35 +1,40 @@
 'use strict';
 
-var ratedHosts = ['amazon.com', 'aa.com', 'southwest.com', 'ashaysheth.com'];
+var topRatedHosts = ['aa.com', 'southwest.com', 'united.com'];
+var highRatedHosts = ['alaskaair.com', 'jetblue.com', 'delta.com', 'hawaiianairlines.com'];
+var lowRatedHosts = ['skywest.com'];
 
-var canvas = document.createElement("canvas");
-var context = canvas.getContext('2d');
-// ...draw to the canvas...
-var imageData = context.getImageData(0, 0, 19, 19);
+var hostsToIcons = [
+    {hosts: topRatedHosts, svgSrc: "assets/iconGreen.svg"},
+    {hosts: highRatedHosts, svgSrc: "assets/iconYellow.svg"},
+    {hosts: lowRatedHosts, svgSrc: "assets/iconRed.svg"},
+];
 
+// Create imageData versions of SVG icons
+hostsToIcons.forEach(map => {
+    let canvas = document.createElement("canvas");
+    let context = canvas.getContext('2d');
+    map.imgData = context.getImageData(0, 0, 19, 19);
+    let img = new Image;
+    img.onload = function(){
+        context.drawImage(img, 0, 0);
+        map.imgData = context.getImageData(0, 0, 128, 128);
+    };
+    img.src = map.svgSrc;
+});
 
 // When the extension is installed or upgraded ...
 chrome.runtime.onInstalled.addListener(function() {
-    // Replace all rules ...
+    // Replace all rules with new rules...
     chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-        // With a new rule ...
-        chrome.declarativeContent.onPageChanged.addRules([
-            {
-                conditions: ratedHosts.map(host => new chrome.declarativeContent.PageStateMatcher({
+        chrome.declarativeContent.onPageChanged.addRules(hostsToIcons.map(hostToIcon => {
+            return {
+                // If the page URL includes the indicated hosts, show the extension's page action and update the icon
+                conditions: hostToIcon.hosts.map(host => new chrome.declarativeContent.PageStateMatcher({
                     pageUrl: {hostContains: host},
                 })),
-                // And shows the extension's page action.
-                actions: [new chrome.declarativeContent.ShowPageAction(), new chrome.declarativeContent.SetIcon({imageData: imageData})]
-            }
-        ]);
+                actions: [new chrome.declarativeContent.ShowPageAction(), new chrome.declarativeContent.SetIcon({imageData: hostToIcon.imgData})]
+            };
+        }));
     });
 });
-
-// chrome.pageAction.setIcon({path: 'assets/iconGreen128.png'});
-
-// chrome.webNavigation.onCompleted.addListener(function() {
-//     chrome.tabs.executeScript({
-//         file: "contentScript.js"
-//     });
-//     alert("This is my favorite website!");
-// }, {url: [{urlMatches : 'https://www.google.com/'}]});
