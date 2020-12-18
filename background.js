955e5c5b4c9386c3b8565a220fcc593f5b786876
Ashay Sheth <ashay.sheth@gmail.com>
2020-12-18 00:59:51 -0500

    updated chrome PageAction icon for top, high, and low scorers (just badge)

--- background.js
+++ background.js
@@ -1,20 +1,20 @@
 'use strict';
 
-var hostsToIcons = [
-    {hosts: topRatedHosts, svgSrc: "assets/iconGreen.svg"},
-    {hosts: highRatedHosts, svgSrc: "assets/iconYellow.svg"},
-    {hosts: lowRatedHosts, svgSrc: "assets/iconRed.svg"},
+var hostsToIcon = [
+    {hosts: topRatedHosts, svgSrc: "assets/badge/greenBadge.svg"},
+    {hosts: highRatedHosts, svgSrc: "assets/badge/yellowBadge.svg"},
+    {hosts: lowRatedHosts, svgSrc: "assets/badge/redBadge.svg"},
 ];
 
-// Create imageData versions of SVG icons
-hostsToIcons.forEach(map => {
+// Create imageData versions of the SVG icons
+hostsToIcon.forEach(map => {
     let canvas = document.createElement("canvas");
     let context = canvas.getContext('2d');
     map.imgData = context.getImageData(0, 0, 19, 19);
     let img = new Image;
     img.onload = function(){
         context.drawImage(img, 0, 0);
-        map.imgData = context.getImageData(0, 0, 128, 128);
+        map.imgData = context.getImageData(0, 0, 64, 64);
     };
     img.src = map.svgSrc;
 });
@@ -24,7 +24,7 @@ chrome.runtime.onInstalled.addListener(function() {
     // Replace all rules with new rules...
     chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
         // Add 3 rules, 1 for each type of top-, high-, and low-rated hosts
-        chrome.declarativeContent.onPageChanged.addRules(hostsToIcons.map(hostToIcon => {
+        chrome.declarativeContent.onPageChanged.addRules(hostsToIcon.map(hostToIcon => {
             return {
                 // If the page URL includes the indicated hosts, show the extension's page action and update the icon
                 conditions: hostToIcon.hosts.map(host => new chrome.declarativeContent.PageStateMatcher({
