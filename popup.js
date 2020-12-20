ba275c0a6bc4f5a14ca030414a5b18381f30f381
Ashay Sheth <ashay.sheth@gmail.com>
2020-12-20 01:37:11 -0500

    popup for unscored sites

--- popup.js
+++ popup.js
@@ -5,15 +5,20 @@ chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
     let urlHost = new URL(activeTab.url).host.split('.').slice(-2).join('.');
     var business = businesses[urlHost];
     // Update popup body class
-    if (business.score >= TOP_THRESHOLD) {
-        document.querySelector("body").className = "top";
-    } else if (business.score >= HIGH_THRESHOLD) {
-        document.querySelector("body").className = "high"
+    if (business) {
+        if (business.score >= TOP_THRESHOLD) {
+            document.querySelector("body").className = "top";
+        } else if (business.score >= HIGH_THRESHOLD) {
+            document.querySelector("body").className = "high"
+        } else {
+            document.querySelector("body").className = "low"
+        }
+        // Add name and score to popup
+        document.querySelectorAll("[data-var='name']").forEach((ele) => { ele.textContent = business.name });
+        document.querySelectorAll("[data-var='score']").forEach((ele) => { ele.textContent = business.score });
+        document.querySelectorAll("[data-var='scorecard-link']").forEach((ele) => { ele.onclick = () => { chrome.tabs.create({url: "https://www.hrc.org/resources/buyers-guide/" + business.name.replace(/'+/g, '').replace(/[^\w.]/g, '-').replace(/-+/g, '-')}); } });
     } else {
-        document.querySelector("body").className = "low"
+        document.querySelector("body").className = "unknown";
+        document.querySelectorAll("[data-var='faq']").forEach((ele) => { ele.onclick = () => { chrome.tabs.create({url: "https://www.hrc.org/resources/corporate-equality-index"}); } });
     }
-    // Add name and score to popup
-    document.querySelectorAll("[data-var='name']").forEach((ele) => { ele.textContent = business.name });
-    document.querySelectorAll("[data-var='score']").forEach((ele) => { ele.textContent = business.score });
-    document.querySelectorAll("[data-var='scorecard-link']").forEach((ele) => { ele.onclick = () => { chrome.tabs.create({url: "https://www.hrc.org/resources/buyers-guide/" + business.name.replace(/'+/g, '').replace(/[^\w.]/g, '-').replace(/-+/g, '-')}); } });
 });
