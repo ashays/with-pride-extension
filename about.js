c13e21db1a7f42adec15e07557fae7c3a4818901
Ashay Sheth <ashay.sheth@gmail.com>
2021-01-02 20:32:20 -0500

    sample businesses cleaned code and updated from onclick to href

--- about.js
+++ about.js
@@ -1,3 +1,25 @@
+// Example businesses
+function getBusinessEle(host) {
+    let node = document.createElement("a");
+    node.title = host;
+    node.href = "http://www." + host;
+    node.target = "_blank";
+    let business = businesses[host];
+    let bucket = "low";
+    if (business.score >= TOP_THRESHOLD) {
+        bucket = "top";
+    } else if (business.score >= HIGH_THRESHOLD) {
+        bucket = "high";
+    }
+    node.innerHTML = "<div class='score " + bucket + "'>" + businesses[host].score + "</div><div class='title'>" + businesses[host].name + "</div><div class='host'>" + host + "</div>";
+    return node;
+}
+
+let hosts = Object.keys(businesses);
+for (let i = 0; i < 12; i++) {
+    document.querySelector('#examples').appendChild(getBusinessEle(hosts[Math.floor(Math.random()*hosts.length)]));
+}
+
 // Analytics
 ga('send', 'pageview', {
     title: "About With Pride",
@@ -20,31 +42,3 @@ document.querySelectorAll('a[target=_blank]').forEach((ele) => {
         });
     }
 });
-
-// Example businesses
-function getBusinessEle(host) {
-    let node = document.createElement("a");
-    node.onclick = () => {
-        ga('send', 'event', {
-            eventCategory: 'Outbound Link',
-            eventAction: 'click',
-            eventLabel: "http://" + host
-        });
-        chrome.tabs.create({url: "http://" + host});
-    }
-    node.title = host;
-    let business = businesses[host];
-    let bucket = "low";
-    if (business.score >= TOP_THRESHOLD) {
-        bucket = "top";
-    } else if (business.score >= HIGH_THRESHOLD) {
-        bucket = "high";
-    }
-    node.innerHTML = "<div class='score " + bucket + "'>" + businesses[host].score + "</div><div class='title'>" + businesses[host].name + "</div><div class='host'>" + host + "</div>";
-    return node;
-}
-
-let hosts = Object.keys(businesses);
-for (let i = 0; i < 12; i++) {
-    document.querySelector('#examples').appendChild(getBusinessEle(hosts[Math.floor(Math.random()*hosts.length)]));
-}
